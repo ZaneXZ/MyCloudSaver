7d0afaace0a78493387b2285fc43d619b49457c9
ZaneXZ <91451117+ZaneXZ@users.noreply.github.com>
2026-01-16 14:04:58 +0800

    Update UserSetting.ts

--- backend/src/models/UserSetting.ts
+++ backend/src/models/UserSetting.ts
@@ -2,14 +2,17 @@ import { DataTypes, Model, Optional } from "sequelize";
 import sequelize from "../config/database";
 import User from "./User";
 
+// 1. 在属性接口中增加 folderId
 interface UserSettingAttributes {
   id: number;
   userId: string;
   cloud115UserId?: string;
   cloud115Cookie: string;
   quarkCookie: string;
+  folderId?: string; // <-- 新增
 }
 
+// 2. 这里的 Optional 会自动包含 folderId，无需改动
 interface UserSettingCreationAttributes extends Optional<UserSettingAttributes, "id"> {}
 
 class UserSetting
@@ -21,6 +24,7 @@ class UserSetting
   public cloud115UserId?: string;
   public cloud115Cookie!: string;
   public quarkCookie!: string;
+  public folderId?: string; // <-- 3. 在类定义中声明属性
 }
 
 UserSetting.init(
@@ -52,11 +56,12 @@ UserSetting.init(
       type: DataTypes.STRING,
       allowNull: true,
     },
+    // 4. 这里的定义是正确的
     folderId: {
       type: DataTypes.STRING,
       allowNull: true,
-      defaultValue: "0"
-    }    
+      defaultValue: "0",
+    },
   },
   {
     sequelize,
